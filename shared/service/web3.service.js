b14f1c09c69f673ee76de92f2e985c4e806593b1
Trung Mai <mai.thieu.trung@gmail.com>
2018-01-20 14:16:00 +0700

    Remove initial contract.

--- shared/service/web3.service.js
+++ shared/service/web3.service.js
@@ -4,9 +4,9 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 const helper = require('../helper');
 const Unit = helper.Unit;
 
-var HuioICOContract = new web3.eth.contract(huioABI, process.env.HuioICOContractAddress, {
-    from: testWallet,
-});
+// var HuioICOContract = new web3.eth.contract(huioABI, process.env.HuioICOContractAddress, {
+//     from: testWallet,
+// });
 
 getBalanceOfAddress = (address, unit = Unit.ether) => web3.fromWei(web3.eth.getBalance(address), Unit.wei).toNumber();
 
